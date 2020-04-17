aa4fe5b5108e272d2462dc50e44586643dda2f96
zhu-chao331 <2659166612@qq.com>
2020-04-17 12:58:56 +0800

    update qifei

---
+++ shiro_3/src/com/lyh/pojo/wuhuqifei.java
@@ -0,0 +1,10 @@
+package com.lyh.pojo;
+
+import java.util.concurrent.ForkJoinPool.ManagedBlocker;
+
+public class wuhuqifei {
+	private void mian() {
+		// TODO Auto-generated method stub
+System.out.println("Îßºþ£¬Æð·É");
+	}
+}
