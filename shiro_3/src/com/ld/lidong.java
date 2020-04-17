fb91025e879523465bc031d47f8e23736f4290d3
lidong6666 <2105007643@qq.com>
2020-04-17 14:38:30 +0800

    aa

--- shiro_3/src/com/ld/lidong.java
+++ shiro_3/src/com/ld/lidong.java
@@ -2,4 +2,15 @@ package com.ld;
 
 public class lidong {
 
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <=9; i++) {
+			System.out.println();
+			for (int j = 1; j <=i; j++) {
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+			}
+			
+		}
+	}
+	
 }
